9fc1bdd9529e48ccc67d7547884538fcb34c8ec4
Waiguru Muriuki <39795674+Waiguru254@users.noreply.github.com>
2025-01-18 23:47:56 -0800

    Update readcapdata.R
    adding janitor to drop cols

--- R/readcapdata.R
+++ R/readcapdata.R
@@ -105,7 +105,7 @@ readcapdata <- function(token, url,fields = NULL, events = NULL, forms = NULL, d
       #### Check whether you need to drop empty columns
       if (isTRUE(drop_empty)) {
         data <- jsonlite::fromJSON(httr::content(httr::POST(url, body = formData, encode = "form"),'text')) |>
-            dplyr::filter(!if_all(everything(), ~ is.na(.) | . == ""))|>
+          janitor::remove_empty('cols')
           ### Make all empty entries to be NA for consistency.
           ### Make is easy in data management to use is.na() without having to ==""
           dplyr::mutate(across(everything(), ~ ifelse(. == "", NA, .)))  |>
