4ac375afbc461c552f4bf0593dc23a638d2a8c6c
Waiguru254 <waigurusamuel@gmail.com>
2025-02-15 19:07:36 -0800

    Updated namespace

--- R/readcapdata.R
+++ R/readcapdata.R
@@ -78,7 +78,7 @@
 readcapdata <- function(token, url,fields = NULL, events = NULL, forms = NULL, drop_empty = FALSE, preprocess_data = FALSE, compact_form= TRUE, list_event_form = FALSE, file_name = NULL) {
   library(dplyr)
   ### checking the options
-  validate_params(preprocess_data, compact_form, drop_empty)
+ # validate_params(preprocess_data, compact_form, drop_empty)
 
   # Displaying the Event and Form names for the project
   if (isTRUE(list_event_form)) {
